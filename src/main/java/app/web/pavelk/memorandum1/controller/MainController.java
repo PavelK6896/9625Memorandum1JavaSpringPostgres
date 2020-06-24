8d3a132c4d3ea49846a7e8d7079e38926bee5a74
PavelK6896 <79208776896@yandex.ru>
2020-06-24 13:15:05 +0300

    "add pagination"

--- src/main/java/app/web/pavelk/memorandum1/controller/MainController.java
+++ src/main/java/app/web/pavelk/memorandum1/controller/MainController.java
@@ -5,6 +5,10 @@ import app.web.pavelk.memorandum1.domain.User;
 import app.web.pavelk.memorandum1.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,17 +42,27 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
-        Iterable<Message> messages;
+    public String main(
+            @RequestParam(required = false,
+                    defaultValue = "") String filter,
+            Model model,
+            //сортировка поубыванию по id
+            @PageableDefault(sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable
+
+    ) {
+        Page<Message> page;
 
         if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
+            page = messageRepo.findByTag(filter, pageable);
         } else {
-            messages = messageRepo.findAll();
+            page = messageRepo.findAll(pageable);
         }
 
-        model.addAttribute("messages", messages);
+
+        model.addAttribute("page", page);//страница
+        model.addAttribute("url", "/main");
         model.addAttribute("filter", filter);
+
         return "main";
     }
 
--- src/main/java/app/web/pavelk/memorandum1/repos/MessageRepo.java
+++ src/main/java/app/web/pavelk/memorandum1/repos/MessageRepo.java
@@ -1,11 +1,15 @@
 package app.web.pavelk.memorandum1.repos;
 
 import app.web.pavelk.memorandum1.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    List<Message> findByTag(String tag);//dsl
+  //  List<Message> findByTag(String tag);//dsl
+    Page<Message> findAll(Pageable pageable);
+    Page<Message> findByTag(String tag, Pageable pageable);//поиск по тегу
 }
